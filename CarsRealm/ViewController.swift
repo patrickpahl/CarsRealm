a992a7962e67fa4ea5d3b051aa72ce7df00b0925
Patrick Pahl <patrick.pahl@lvco2vd9vyhtd6.local>
2018-05-01 13:56:58 -0500

    Initial Commit

--- CarsRealm/ViewController.swift
+++ CarsRealm/ViewController.swift
@@ -1,23 +1,9 @@
-//
-//  ViewController.swift
-//  CarsRealm
-//
-//  Created by Patrick Pahl on 5/1/18.
-//  Copyright © 2018 Patrick Pahl. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
